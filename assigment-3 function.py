8ef8db5156a3f5735cecbaefb1b88b53795d8c9f
deekondavarukalyan <138508442+deekondavarukalyan@users.noreply.github.com>
2023-08-04 12:08:10 +0530

    Add files via upload

---
+++ assigment-3 function.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# Write a python function to sum all the number is a list
+
+
+# In[1]:
+
+
+list1=eval(input(("enter the list:" )))
+def sum_list(list1):
+    sum_total=0
+    for element in list1:
+        sum_total=sum_total+element
+    return sum_total
+sum_list(list1)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
